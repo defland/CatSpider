a75cd28754bdae47e5fb2daac019d0d70d22e1b8
defland <defland@MBP.local>
2018-01-25 16:27:56 +0800

    added:  redis queue done

--- core/setting.py
+++ core/setting.py
@@ -1,8 +1,14 @@
 # coding:utf-8
 
 
-
 # use-redis
 USE_REDIS = True
 
+# redis address port password
+REDIS_DICT = {
 
+    'ADDRESS': 'teamof.codes',
+    'PORT': 6379,
+    'PASSWROD': 123456,
+    'DB':0
+}
--- core/xqueue.py
+++ core/xqueue.py
@@ -14,31 +14,62 @@ by yg
 
 """
 
-from . import setting
+
+import setting
+import redis
 
 
-class Queue:
+class Xqueue:
     """
     1、增删改查
     2、是否采用Redis存储
     3、默认采用广度优先的队列,新添加的url放入queue尾部
     """
 
-    def __init__(self):
+    def __init__(self, queue_name, namespace='Xqueue', **redis_kwargs):
         self.use_redis = setting.USE_REDIS
-        self.items = []
+        # reids queue namespace
+        self.key = '%s:%s' % (namespace, queue_name)
+        self.__redis_r = self.init_redis()  # use redis
+
+    def init_redis(self):
+        if self.use_redis:
+            pool = pool = redis.ConnectionPool(
+                host=setting.REDIS_DICT.get('ADDRESS'),
+                port=setting.REDIS_DICT.get('PORT'),
+                password=setting.REDIS_DICT.get('PASSWROD'),
+                db=setting.REDIS_DICT.get('DB')
+            )
+            r = redis.Redis(connection_pool=pool)
+            return r
 
     def isEmpty(self):  # 为空
-        return self.items == []
+        return self.size() == 0
 
-    def takeout(self, item):  # 顶部取出
-        self.items.insert(0, item)
+    def insert(self, item):  # 顶部取出
+        try:
+            self.__redis_r.rpush(self.key, item)
+            return True
+        except Exception as e:
+            print(e)
+            return False
 
-    def putin(self):  # 顶部删除
-        return self.items.pop()
+    def get(self, block=True, timeout=None):  # 顶部删除
+        # 分阻塞模式和非阻塞模式
+        if block:
+            return self.__redis_r.blpop(self.key, timeout=timeout)
+        else:
+            return self.__redis_r.lpop(self.key)
+    def get_no_wait(self):  # 顶部删除
+        return self.get(block=False)
 
     def size(self):
-        return len(self.items)  # 大小，高度
+        return self.__redis_r.llen(self.key)  # 大小，高度
 
     def show(self):
         print(self.items)
+
+
+x = Xqueue('test')
+
+print(x.size())
